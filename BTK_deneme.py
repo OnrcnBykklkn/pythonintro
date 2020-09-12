fe56c117d2241891242af0c3f5957937472f9c23
OnrcnBykklkn <65039951+OnrcnBykklkn@users.noreply.github.com>
2020-09-13 02:23:51 +0300

    Add files via upload

---
+++ BTK_deneme.py
@@ -0,0 +1,63 @@
+"""
+pi = 3.14
+
+r = float(input("Yari Cap: "))
+
+alan = pi * (r ** 2)
+cevre = 2 * pi * r
+
+alan = float(alan)
+cevre = float(cevre)
+
+print("alan", alan)
+print("cevre", cevre)
+"""
+
+# Karakter dizileri.
+
+"""
+name = input(" name: ")
+surname = input(" surname: ")
+age = input(" age: ")
+job = input(" job: ")
+
+result = "Benim adım " + name + " soyadım " + surname + str(age) + " yaşındayım." + "Mesleğim " + job
+
+print(result)
+"""
+"""
+name = input(" name: ")
+surname = input(" surname: ")
+age = input(" age: ")
+job = input(" job: ")
+
+result = f"Benim adım {name} soyadım {surname}. {age} yaşındayım. Mesleğim {job}."
+
+print(result)
+
+"""
+"""
+s = "Helloworld"
+
+s.replace("w","W")
+
+result = s
+
+print(result)
+"""
+#String metotları
+
+"""
+password = "Şifreniz: " + input("Password: ")
+
+password = password.upper()
+password = password.lower()
+password = password.title()
+password = password.split()
+password = password.strip()
+password = password.find
+
+print(password)
+
+"""
+
